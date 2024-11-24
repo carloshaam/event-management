681265c53c2456625ac708717ebfe0ab83391d2d
Carlos Moreira <carloshaam@gmail.com>
2024-11-24 16:58:23 -0300

    🔥 remove broadcasting from event and add notification listener
    Refactored `EventRegistered` by removing broadcasting functionality. Introduced `SendNotificationEventDraft` listener to handle event registration notifications with queuing and failure handling.

--- app/Events/EventRegistered.php
+++ app/Events/EventRegistered.php
@@ -5,11 +5,7 @@ declare(strict_types=1);
 namespace App\Events;
 
 use App\Models\Event;
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
@@ -20,17 +16,7 @@ class EventRegistered
     /**
      * Create a new event instance.
      */
-    public function __construct(public Event $event) {}
-
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return array<int, \Illuminate\Broadcasting\Channel>
-     */
-    public function broadcastOn(): array
-    {
-        return [
-            new PrivateChannel('channel-name'),
-        ];
-    }
+    public function __construct(
+        public Event $event,
+    ) {}
 }
---
+++ app/Listeners/SendNotificationEventDraft.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Listeners;
+
+use App\Events\EventRegistered;
+use App\Notifications\EventDraftRegistrationNotification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
+use Throwable;
+
+class SendNotificationEventDraft implements ShouldQueue
+{
+    use InteractsWithQueue;
+
+    /**
+     * The time (seconds) before the job should be processed.
+     *
+     * @var int
+     */
+    public int $delay = 60;
+
+    /**
+     * Create the event listener.
+     */
+    public function __construct() {}
+
+    /**
+     * Handle the event.
+     */
+    public function handle(EventRegistered $event): void
+    {
+        info(json_encode($event->event->id));
+        $event->event->user->notify(new EventDraftRegistrationNotification($event->event));
+    }
+
+    /**
+     * Determine whether the listener should be queued.
+     */
+    public function shouldQueue(EventRegistered $event): bool
+    {
+        return $event->event->isDraft();
+    }
+
+    /**
+     * Handle a job failure.
+     */
+    public function failed(EventRegistered $event, Throwable $exception): void
+    {
+        Log::error(
+            'Failed to send notification after event registration', [
+                'event_id' => $event->event->id,
+                'error' => $exception->getMessage()
+            ]
+        );
+    }
+}
